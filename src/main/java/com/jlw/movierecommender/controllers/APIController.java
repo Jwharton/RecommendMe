36cd3cca2207bc2dec42c6311c92bf641b59a245
Jennifer Wharton <jwharto1@gmail.com>
2020-05-11 22:30:44 -0400

    Updated APIController logic to properly handle get request mappings.  Updated bootstrap and thymeleaf templates.  Closes #5.

--- src/main/java/com/jlw/movierecommender/controllers/APIController.java
+++ src/main/java/com/jlw/movierecommender/controllers/APIController.java
@@ -1,40 +1,36 @@
 package com.jlw.movierecommender.controllers;
 
+import com.jlw.movierecommender.restapis.model.Media;
 import com.jlw.movierecommender.restapis.model.MediaSearchResult;
 import com.jlw.movierecommender.restapis.model.Search;
 import com.jlw.movierecommender.services.APIService;
-import com.jlw.movierecommender.utilities.PropertyFileReader;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"/", "/index"})
 public class APIController {
 
-    private  APIService apiService;
-    private  MediaSearchResult mediaSearchResult;
+    private APIService<Media> apiService;
+    private MediaSearchResult  mediaSearchResult;
 
-    public APIController(APIService apiService, MediaSearchResult mediaSearchResult) {
+    public APIController(APIService<Media>  apiService, MediaSearchResult mediaSearchResult) {
         this.apiService = apiService;
         this.mediaSearchResult = mediaSearchResult;
     }
 
-
-    @GetMapping(value="")
-    public String searchResult(Model model){
-        model.addAttribute("search", new Search());
-        model.addAttribute("movies", mediaSearchResult.getResults());
+    @GetMapping({"/", "/index", "{pageRequested}"})
+    public String main(@PathVariable(required = false) String pageRequested, Search searchObj, Model model) {
+        if (null == searchObj.getKeyword()) {
+            model.addAttribute("search", new Search());
+        } else {
+            mediaSearchResult.setResults(apiService.searchByKeyword(searchObj.getKeyword()));
+            model.addAttribute("movies", mediaSearchResult.getResults());
+            return pageRequested;
+        }
         return "index";
     }
-
-    @PostMapping(value ="")
-    public String displaySearchResults(Search searchObj) {
-
-        mediaSearchResult.setResults(apiService.searchByKeyword(searchObj.getKeyword()));
-        return "redirect:/";
-    }
-
 }
